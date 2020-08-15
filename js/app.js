8a598c31f0f8e1de72246b8968d543854f033008
steveo1978 <steve.r.marical@outlook.com>
2020-08-15 16:27:43 -0700

    completed lab7

--- js/app.js
+++ js/app.js
@@ -9,94 +9,159 @@ function getRandomCustNumber(min, max) {
   //The maximum is inclusive and the minimum is inclusive
 }
 
+function Store(name, minCust, maxCust, avgCookiesPerHour) {
+  this.name = name;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCookiesPerHour = avgCookiesPerHour;
+  this.custPerHourArray = [];
+  this.totalDailySales = 0;
+  //this.calcCustPerHour();
+}
+
+
+var seattle = new Store('seattle', 23, 65, 6.3);
+Store.prototype.calcCustPerHour = function () {
+  for (var i = 0; i < hours.length; i++) {
+    var hourlyCust = Math.ceil(getRandomCustNumber(this.minCust, this.maxCust) * this.avgCookiesPerHour);
+    this.custPerHourArray.push(hourlyCust);
+    this.totalDailySales = hourlyCust + this.totalDailySales;
+  }
+};
+
+
 //create Seattle object literal
-var seattle = {
-  name: 'Seattle',
-  minCust: 23,
-  maxCust: 65,
-  avgCookiesPerHour: 6.3,
-  custPerHourArray: [],
-  totalDailySales: 0,
-  //caluc number of customers per hour by generating a random number of customers and push that number of customers into cust per hour array.
-  calcCustPerHour: function () {
-    for (var i = 0; i < hours.length; i++) {
-      var hourlyCust = Math.ceil(getRandomCustNumber(this.minCust, this.maxCust) * this.avgCookiesPerHour);
-      this.custPerHourArray.push(hourlyCust);
-      this.totalDailySales = hourlyCust + this.totalDailySales;
-      //console.log(this.totalDailySales); 
-    }
+// var seattle =
+//   name: 'Seattle',
+//   minCust: 23,
+//   maxCust: 65,
+//   avgCookiesPerHour: 6.3,
+//   custPerHourArray: 
+//   totalDailySales: 0,
+//   //caluc number of customers per hour by generating a random number of customers and push that number of customers into cust per hour array.
+//function seattle=(calcCustPerHour) {
+//console.log(this.totalDailySales);
+
+
+// function seattle(seattle) //{
+//   console.log('seattle store', seattle);
+//   //};
+//}
+
+
+
+// function sayHello() {
+//   console.log('hello');
+// };
+// sayHello();
+
+// function sayHelloUser(user) {
+//  console.log('hello ' + user + '!');
+// }
+// sayHelloUser('billy');
+
+
+
+
+var tokyo = new Store('tokyo', 3, 24, 1.2);
+Store.prototype.calcCustPerHour = function () {
+  for (var i = 0; i < hours.length; i++) {
+    var hourlyCust = Math.ceil(getRandomCustNumber(this.minCust, this.maxCust) * this.avgCookiesPerHour);
+    this.custPerHourArray.push(hourlyCust);
+    this.totalDailySales = hourlyCust + this.totalDailySales;
+
+    // var tokyo = {
+    //   name: 'Tokyo',
+    //   minCust: 3,
+    //   maxCust: 24,
+    //   avgCookiesPerHour: 1.2,
+    //   custPerHourArray: [],
+    //   totalDailySales: 0,
+    //   //caluc number of customers per hour by generating a random number of customers and push that number of customers into cust per hour array.
+    //   calcCustPerHour: function () {
+    //     for (var i = 0; i < hours.length; i++) {
+    //       var hourlyCust = Math.ceil(getRandomCustNumber(this.minCust, this.maxCust) * this.avgCookiesPerHour);
+    //       this.custPerHourArray.push(hourlyCust);
+    //       this.totalDailySales = hourlyCust + this.totalDailySales;
+    //console.log(this.totalDailySales);
+
   }
 };
 
-var tokyo = {
-  name: 'Tokyo',
-  minCust: 3,
-  maxCust: 24,
-  avgCookiesPerHour: 1.2,
-  custPerHourArray: [],
-  totalDailySales: 0,
-  //caluc number of customers per hour by generating a random number of customers and push that number of customers into cust per hour array.
-  calcCustPerHour: function () {
-    for (var i = 0; i < hours.length; i++) {
-      var hourlyCust = Math.ceil(getRandomCustNumber(this.minCust, this.maxCust) * this.avgCookiesPerHour);
-      this.custPerHourArray.push(hourlyCust);
-      this.totalDailySales = hourlyCust + this.totalDailySales;
-      //console.log(this.totalDailySales); 
-    }
+var dubai = new Store('dubai', 11, 38, 3.7);
+Store.prototype.calcCustPerHour = function () {
+  for (var i = 0; i < hours.length; i++) {
+    var hourlyCust = Math.ceil(getRandomCustNumber(this.minCust, this.maxCust) * this.avgCookiesPerHour);
+    this.custPerHourArray.push(hourlyCust);
+    this.totalDailySales = hourlyCust + this.totalDailySales;
+
+
+    // var dubai = {
+    //   name: 'Dubai',
+    //   minCust: 11,
+    //   maxCust: 38,
+    //   avgCookiesPerHour: 3.7,
+    //   custPerHourArray: [],
+    //   totalDailySales: 0,
+    //   //caluc number of customers per hour by generating a random number of customers and push that number of customers into cust per hour array.
+    //   calcCustPerHour: function () {
+    //     for (var i = 0; i < hours.length; i++) {
+    //       var hourlyCust = Math.ceil(getRandomCustNumber(this.minCust, this.maxCust) * this.avgCookiesPerHour);
+    //       this.custPerHourArray.push(hourlyCust);
+    //       this.totalDailySales = hourlyCust + this.totalDailySales;
+    //       //console.log(this.totalDailySales);
+    //     }
   }
 };
 
-var dubai = {
-  name: 'Dubai',
-  minCust: 11,
-  maxCust: 38,
-  avgCookiesPerHour: 3.7,
-  custPerHourArray: [],
-  totalDailySales: 0,
-  //caluc number of customers per hour by generating a random number of customers and push that number of customers into cust per hour array.
-  calcCustPerHour: function () {
-    for (var i = 0; i < hours.length; i++) {
-      var hourlyCust = Math.ceil(getRandomCustNumber(this.minCust, this.maxCust) * this.avgCookiesPerHour);
-      this.custPerHourArray.push(hourlyCust);
-      this.totalDailySales = hourlyCust + this.totalDailySales;
-      //console.log(this.totalDailySales); 
-    }
+var paris = new Store('paris', 20, 38, 2.3);
+Store.prototype.calcCustPerHour = function () {
+  for (var i = 0; i < hours.length; i++) {
+    var hourlyCust = Math.ceil(getRandomCustNumber(this.minCust, this.maxCust) * this.avgCookiesPerHour);
+    this.custPerHourArray.push(hourlyCust);
+    this.totalDailySales = hourlyCust + this.totalDailySales;
+
+    // var paris = {
+    //   name: 'Paris',
+    //   minCust: 20,
+    //   maxCust: 38,
+    //   avgCookiesPerHour: 2.3,
+    //   custPerHourArray: [],
+    //   totalDailySales: 0,
+    //   //caluc number of customers per hour by generating a random number of customers and push that number of customers into cust per hour array.
+    //   calcCustPerHour: function () {
+    //     for (var i = 0; i < hours.length; i++) {
+    //       var hourlyCust = Math.ceil(getRandomCustNumber(this.minCust, this.maxCust) * this.avgCookiesPerHour);
+    //       this.custPerHourArray.push(hourlyCust);
+    //       this.totalDailySales = hourlyCust + this.totalDailySales;
+    //       //console.log(this.totalDailySales);
+    //     }
   }
 };
 
-var paris = {
-  name: 'Paris',
-  minCust: 20,
-  maxCust: 38,
-  avgCookiesPerHour: 2.3,
-  custPerHourArray: [],
-  totalDailySales: 0,
-  //caluc number of customers per hour by generating a random number of customers and push that number of customers into cust per hour array.
-  calcCustPerHour: function () {
-    for (var i = 0; i < hours.length; i++) {
-      var hourlyCust = Math.ceil(getRandomCustNumber(this.minCust, this.maxCust) * this.avgCookiesPerHour);
-      this.custPerHourArray.push(hourlyCust);
-      this.totalDailySales = hourlyCust + this.totalDailySales;
-      //console.log(this.totalDailySales); 
-    }
-  }
-};
+var lima = new Store('lima', 2, 16, 4.6);
+Store.prototype.calcCustPerHour = function () {
+  for (var i = 0; i < hours.length; i++) {
+    var hourlyCust = Math.ceil(getRandomCustNumber(this.minCust, this.maxCust) * this.avgCookiesPerHour);
+    this.custPerHourArray.push(hourlyCust);
+    this.totalDailySales = hourlyCust + this.totalDailySales;
 
-var lima = {
-  name: 'Lima',
-  minCust: 2,
-  maxCust: 16,
-  avgCookiesPerHour: 4.6,
-  custPerHourArray: [],
-  totalDailySales: 0,
-  //caluc number of customers per hour by generating a random number of customers and push that number of customers into cust per hour array.
-  calcCustPerHour: function () {
-    for (var i = 0; i < hours.length; i++) {
-      var hourlyCust = Math.ceil(getRandomCustNumber(this.minCust, this.maxCust) * this.avgCookiesPerHour);
-      this.custPerHourArray.push(hourlyCust);
-      this.totalDailySales = hourlyCust + this.totalDailySales;
-      //console.log(this.totalDailySales); 
-    }
+
+    // var lima = {
+    //   name: 'Lima',
+    //   minCust: 2,
+    //   maxCust: 16,
+    //   avgCookiesPerHour: 4.6,
+    //   custPerHourArray: [],
+    //   totalDailySales: 0,
+    //   //caluc number of customers per hour by generating a random number of customers and push that number of customers into cust per hour array.
+    //   calcCustPerHour: function () {
+    //     for (var i = 0; i < hours.length; i++) {
+    //       var hourlyCust = Math.ceil(getRandomCustNumber(this.minCust, this.maxCust) * this.avgCookiesPerHour);
+    //       this.custPerHourArray.push(hourlyCust);
+    //       this.totalDailySales = hourlyCust + this.totalDailySales;
+    //       //console.log(this.totalDailySales);
+    //     }
   }
 };
 
@@ -126,9 +191,3 @@ for (var j = 0; j < cities.length; j++) {
   liEleTotal.textContent = `Total: ${cities[j].totalDailySales}`;
   ulEle.appendChild(liEleTotal);
 }
-
-
-
-
-
-
